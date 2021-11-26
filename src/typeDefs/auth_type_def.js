77d18a23c50804780902c5a2a67315690c98524f
luismsanchez <luism.sanchez13@gmail.com>
2021-11-25 19:22:10 -0500

    auth typedefs

---
+++ src/typeDefs/auth_type_def.js
@@ -0,0 +1,18 @@
+const { gql } = require('apollo-server');
+
+const authTypeDefs = gql `
+    input LoginInput {
+        username: String!
+        password: String!
+    }
+
+    type Token {
+        key: String!
+    }
+
+    type Mutation {
+        login(credentials: LoginInput!): Token!
+    }
+`;
+
+module.exports = authTypeDefs;
